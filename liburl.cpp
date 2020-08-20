bb9ca7f2f697aa5cb7c6e0fdba4dbe2259109179
Hoa Thiên Vũ <hoathienvu8x@gmail.com>
2020-08-20 10:15:13 +0700

    Add liburl encode + decode function

---
+++ liburl.cpp
@@ -0,0 +1,47 @@
+#include "liburl.h"
+#include <vector>
+
+inline void hexchar(unsigned char c, unsigned char &hex1, unsigned char &hex2) {
+    hex1 = c / 16;
+    hex2 = c % 16;
+    hex1 += hex1 <= 9 ? '0' : 'a' - 10;
+    hex2 += hex2 <= 9 ? '0' : 'a' - 10;
+}
+
+std::string urlencode(std::string s) {
+    const char *str = s.c_str();
+    std::vector<char> v(s.size());
+    v.clear();
+    for (size_t i = 0, l = s.size(); i < l; i++) {
+        char c = str[i];
+        if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '-' || c == '_' || c == '.' || c == '!' || c == '~' || c == '*' || c == '\'' || c == '(' || c == ')') {
+            v.push_back(c);
+        } else if (c == ' ') {
+            v.push_back('+');
+        } else {
+            v.push_back('%');
+            unsigned char d1, d2;
+            hexchar(c, d1, d2);
+            v.push_back(d1);
+            v.push_back(d2);
+        }
+    }
+    return std::string(v.cbegin(), v.cend());
+}
+std::string urldecode(std::string s) {
+    std::string ret;
+    char ch;
+    int i, ii, len = s.length();
+    for (i=0; i < len; i++){
+        if(s[i] != '%'){
+            if(s[i] == '+') ret += ' ';
+            else ret += s[i];
+        } else {
+            sscanf(s.substr(i + 1, 2).c_str(), "%x", &ii);
+            ch = static_cast<char>(ii);
+            ret += ch;
+            i = i + 2;
+        }
+    }
+    return ret;
+}
---
+++ liburl.h
@@ -0,0 +1,9 @@
+#ifndef _LIBURL_H
+#define _LIBURL_H
+
+#include <string>
+
+std::string urlencode(std::string s);
+std::string urldecode(std::string s);
+
+#endif
